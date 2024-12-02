27b2e8744d93235b3aaa57a891be9dd00f680645
Muchlis00 <muchlisriski@gmail.com>
2024-12-02 15:32:52 +0700

    index tenaga pendidik
    done with templating

--- resources/views/tenaga-pendidik/index.blade.php
+++ resources/views/tenaga-pendidik/index.blade.php
@@ -1,43 +1,66 @@
 @extends('layouts.navbar')
 @section('content')
 
-<div class="container">
-    <h1>Data Tenaga Pendidik</h1>
+<div class="container-fluid">
+    <h1 class="h3 mb-2 text-gray-800">Tenaga Pendidik</h1>
     <a href="{{ route('tenaga-pendidik.create') }}" class="btn btn-primary">Tambah Guru</a>
-    <table class="table table-bordered mt-3">
-        <thead>
-            <table border="1">
-                <tr>
-                    <th>NIP</th>
-                    <th>Nama</th>
-                    <th>Tempat Lahir</th>
-                    <th>Tanggal Lahir</th>
-                    <th>Jenis Kelamin</th>
-                    <th>Jabatan</th>
-                    <th>Aksi</th>
-                </tr>
-        </thead>
-        <tbody>
-            @foreach($tenagaPendidik as $tp)
-            <tr>
-                <td>{{ $tp->nip }}</td>
-                <td>{{ $tp->nama_guru }}</td>
-                <td>{{ $tp->tempat_guru }}</td>
-                <td>{{ $tp->tgl_guru }}</td>
-                <td>{{ $tp->jk_guru }}</td>
-                <td>{{ $tp->jabatan }}</td>
-                <td>
-                    <a href="{{ route('tenaga-pendidik.show', $tp->id_pendidik) }}">Lihat</a>
-                    <a href="{{ route('tenaga-pendidik.edit', $tp->id_pendidik) }}" class="btn btn-warning">Edit</a>
-                    <form action="{{ route('tenaga-pendidik.destroy', $tp->id_pendidik) }}" method="POST" style="display:inline;">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-danger">Hapus</button>
-                    </form>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <br><br>
+
+
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">DataTable Tenaga Pendidik</h6>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>NIP</th>
+                            <th>Nama</th>
+                            <th>Tempat Lahir</th>
+                            <th>Tanggal Lahir</th>
+                            <th>Jenis Kelamin</th>
+                            <th>Jabatan</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tfoot>
+                        <tr>
+                            <th>NIP</th>
+                            <th>Nama</th>
+                            <th>Tempat Lahir</th>
+                            <th>Tanggal Lahir</th>
+                            <th>Jenis Kelamin</th>
+                            <th>Jabatan</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </tfoot>
+                    <tbody>
+                        @foreach($tenagaPendidik as $tp)
+                        <tr>
+                            <td>{{ $tp->nip }}</td>
+                            <td>{{ $tp->nama_guru }}</td>
+                            <td>{{ $tp->tempat_guru }}</td>
+                            <td>{{ $tp->tgl_guru }}</td>
+                            <td>{{ $tp->jk_guru }}</td>
+                            <td>{{ $tp->jabatan }}</td>
+                            <td>
+                                <a href="{{ route('tenaga-pendidik.show', $tp->id_pendidik) }}">Lihat</a>
+                                <a href="{{ route('tenaga-pendidik.edit', $tp->id_pendidik) }}" class="btn btn-warning">Edit</a>
+                                <form action="{{ route('tenaga-pendidik.destroy', $tp->id_pendidik) }}" method="POST" style="display:inline;">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger">Hapus</button>
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
 </div>
 @endsection
