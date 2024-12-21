1d4a042d13478b5095b005f02de703d4e49d48f5
Rizki Aprita <vallerydelexy@gmail.com>
2024-12-21 16:59:40 +0700

    fix navigations

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -7,6 +7,7 @@
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                         
                     </a>
                 </div>
 
@@ -14,6 +15,11 @@
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex"> <!-- hidden space-x-8 sm:-my-px sm:ms-10 sm:flex -->
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
+                    </x-nav-link>
+                </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex"> <!-- hidden space-x-8 sm:-my-px sm:ms-10 sm:flex -->
+                    <x-nav-link :href="route('tenaga-pendidik.index')">
+                        {{ __('Tenaga Pendidik') }}
                     </x-nav-link>
                 </div>
             </div>
--- resources/views/tenaga-pendidik/create.blade.php
+++ resources/views/tenaga-pendidik/create.blade.php
@@ -1,8 +1,7 @@
-
 <div class="container">
     @if(session()->has('success'))
     <p>sukses</p>
-    @endif 
+    @endif
     <h1>Tambah Guru</h1>
     <form action="{{ route('tenaga-pendidik.store') }}" method="POST">
         @csrf
@@ -23,15 +22,21 @@
             <input type="date" name="tgl_guru" class="form-control" id="tgl_guru" required>
         </div>
         <div class="mb-3">
-            <label for="jk_guru" class="form-label">jk Guru</label>
-            <input type="text" name="jk_guru" class="form-control" id="jk_guru" required>
+            <label for="jk_guru">jk Guru</label>
+            <select name="jk_guru" id="jk_guru" required>
+                <option value="Laki-Laki">Laki-laki</option>
+                <option value="Perempuan">Perempuan</option>
+            </select>
         </div>
         <div class="mb-3">
             <label for="jabatan" class="form-label">jabatan Guru</label>
-            <input type="text" name="jabatan" class="form-control" id="jabatan" required>
+            <select name="jabatan" id="jabatan" required>
+            <option value="Guru">Guru</option>
+            <option value="Bendahara">Bendahara</option>
+            <option value="Kepsek">Kepsek</option>
+        </select>
         </div>
         <!-- Tambahkan input untuk tempat_lahir_guru, tgl_lahir_guru, jeniskelamin_guru, jabatan -->
         <button type="submit" class="btn btn-primary">Simpan</button>
     </form>
 </div>
-
